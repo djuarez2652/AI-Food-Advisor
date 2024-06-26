fce244f38a47e170dfe9ada2c4899b3565f69b04
djuarez2652 <djuarez0440@gmail.com>
2024-06-26 18:31:41 +0000

    updated test.py

--- week2_project/test.py
+++ week2_project/test.py
@@ -1,2 +1,28 @@
 import unittest
-from main import get_user_info, 
+from main import *
+
+class TestMain(unittest.TestCase):
+
+    def setUp(self):
+        self.user = {
+                    "name": "Rob",
+                    "age": 18,
+                    "currWeight": 140,
+                    "goalWeight": 130,
+                    "reason": "To lose weight"
+                    }
+
+    def test_get_user_info(self):        
+        self.assertEqual(self.get_user_info(), self.user)
+
+    def test_print_database(self):
+        self.print_database()
+
+    def test_input_userdata_into_db(self):
+        self.input_userdata_into_db(self.user)
+
+    def test_print_user_in_db(self):
+        self.print_user_in_db()
+
+    def test_get_username(self):
+        self.assertEqual(self.get)
