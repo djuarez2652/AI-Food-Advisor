16ae61ce3818ae80897c4d9174faec77c5be94f3
djuarez2652 <djuarez0440@gmail.com>
2024-06-27 05:33:46 +0000

    caches calories of food from API to improve efficiency

--- week2_project/food.py
+++ week2_project/food.py
@@ -1,4 +1,5 @@
 import requests
+import sqlite3
 import json
 import pandas as pd
 import os
@@ -9,17 +10,33 @@ a list of INTs of their respective amount of calories
 for each item; if not found in API it returns None. 
 Appends the total number of calories at the end
 """
-
 def getCalories( lst_of_foods ):
 
+    engine = sqlite3.connect("food.db")
+    cursor = engine.cursor()
+    create_table = """
+        CREATE TABLE IF NOT EXISTS food (
+            name VARCHAR(255) NOT NULL,
+            calories INT NOT NULL,
+            PRIMARY KEY(name)
+        );
+    """
+    cursor.execute(create_table)
+    engine.commit()
+
+
     lst_of_cals = []
-    cache = {}
     total = 0
 
     for food in lst_of_foods:
-        if food in cache:
-            total += cache[food]
-            lst_of_cals.append(cache[food])
+
+        db_query = "SELECT calories FROM food WHERE name = ?"
+        cursor.execute(db_query, (food.lower(),))
+        result = cursor.fetchone()
+
+        if result:
+            total += result[0]
+            lst_of_cals.append(result[0])
             continue
 
         url = "https://food-nutrition-information.p.rapidapi.com/foods/search"
@@ -51,12 +68,17 @@ def getCalories( lst_of_foods ):
                 cals = nutrients['value']
                 if cals > 400: # some serving sizes are too big
                     cals //= 5
-                cache[food] = cals
+                sql_insert = "INSERT INTO food (name, calories) VALUES (?,?)"
+                cursor.execute(sql_insert, (food.lower(),cals))
+                engine.commit()
                 total += cals
                 break
 
         lst_of_cals.append(cals)
     
     lst_of_cals.append(total)
+
+    engine.commit()
+    engine.close()
 
     return lst_of_cals
--- week2_project/prompt.py
+++ week2_project/prompt.py
@@ -17,7 +17,7 @@
 # ... (Other OPTIONAL options)
 # """
 prompt = """
-You are a health advisor and are giving healthy food recommendations. Give 3 meals (breakfast, lunch, dinner) and include the name of the meal along with a short ingredients list as comma separated values. If prompted to give more or different meals, then give DIFFERENT meals that have NOT been stated already. Ensure that the meals will help reach the goal that the user inputs. Give some options for each meal, at least 2 and at most 3. Format your response like so -- do NOT include any other filler words or sentences and list the options with NO numbers, start with the name: 
+You are a health advisor and are giving healthy food recommendations. Give 3 meals (breakfast, lunch, dinner) and include the name of the meal along with a short ingredients list as comma separated values and do NOT include any parenthesis to explain ingredients. Write the ingredients as formally as possible. If prompted to give more or different meals, then give DIFFERENT meals that have NOT been stated already. Ensure that the meals will help reach the goal that the user inputs. Give some options for each meal, at least 2 and at most 3. Format your response like so -- do NOT include any other filler words or sentences and list the options with NO numbers, start with the name: 
 
 Breakfast:
 - name | item1, item2, item3
